d4186ae70b53403a4f521e560ee4b18a349eb586
Mrityunjay Tripathi <mrityunjay.tr@gmail.com>
2022-07-27 01:27:46 +0530

    designing basic structure for the project

---
+++ tests/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// #define CATCH_CONFIG_MAIN  // catch.hpp will define main()
+#define CATCH_CONFIG_RUNNER  // we will define main()
+#include "catch.hpp"
+
+int main(int argc, char** argv)
+{
+  return Catch::Session().run(argc, argv);
+}
